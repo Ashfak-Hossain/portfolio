62edcc883536305f03fa1a7e5d2843431897fb96
Ashfak hossain Evan <evan1234.ek@gmail.com>
2026-07-07 03:45:06 +0600

    feat: remove OneCut section elated content and styles

--- src/content/index.ts
+++ src/content/index.ts
@@ -6,7 +6,6 @@ export { about } from './about';
 export { poster } from './poster';
 export { stats } from './stats';
 export { projects, workBlurb, workHeadingJp } from './work';
-export { oneCut } from './oneCut';
 export { skills, skillsHeading, skillsHeadingJp, skillsBlurb } from './skills';
 export { connect } from './connect';
 export { contact, footer } from './contact';
